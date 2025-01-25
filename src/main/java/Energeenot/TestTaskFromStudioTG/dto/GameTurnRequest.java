1f0a62584e24efa2d8f79b885b5dab387b1bbb1b
Energeenot <abramov.pandos@yandex.ru>
2025-01-25 17:20:49 +0300

    добавил аннотации чтобы дто соответствовало snake case

--- src/main/java/Energeenot/TestTaskFromStudioTG/dto/GameTurnRequest.java
+++ src/main/java/Energeenot/TestTaskFromStudioTG/dto/GameTurnRequest.java
@@ -1,5 +1,6 @@
 package Energeenot.TestTaskFromStudioTG.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -8,6 +9,7 @@ import lombok.Data;
 public class GameTurnRequest {
 
     @NotNull
+    @JsonProperty("game_id")
     private String gameId;
     @Min(0)
     private int col;
