b67eca1574f2273341d15b3c29d326282ca01a36
Energeenot <abramov.pandos@yandex.ru>
2025-01-25 17:20:07 +0300

    добавил аннотации чтобы дто соответствовало snake case

--- src/main/java/Energeenot/TestTaskFromStudioTG/dto/GameInfoResponse.java
+++ src/main/java/Energeenot/TestTaskFromStudioTG/dto/GameInfoResponse.java
@@ -1,14 +1,17 @@
 package Energeenot.TestTaskFromStudioTG.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 @Data
 @Builder
 public class GameInfoResponse {
 
+    @JsonProperty("game_id")
     private String gameId;
     private int width;
     private int height;
+    @JsonProperty("mines_count")
     private int minesCount;
     private boolean completed;
     private String[][] field;
