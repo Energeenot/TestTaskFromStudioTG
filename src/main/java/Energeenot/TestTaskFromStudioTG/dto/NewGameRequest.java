43d22d7b5fdf23400d5770ff7ce8021f9b346d08
Energeenot <abramov.pandos@yandex.ru>
2025-01-25 17:21:39 +0300

    добавил аннотации чтобы дто соответствовало snake case, и изменил сообщения, если данные не проходят валидацию

--- src/main/java/Energeenot/TestTaskFromStudioTG/dto/NewGameRequest.java
+++ src/main/java/Energeenot/TestTaskFromStudioTG/dto/NewGameRequest.java
@@ -1,6 +1,7 @@
 package Energeenot.TestTaskFromStudioTG.dto;
 
 import Energeenot.TestTaskFromStudioTG.validation.annotation.ValidGameRequest;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
@@ -12,15 +13,16 @@ public class NewGameRequest {
 
     @NotNull
     @Min(value = 2, message = "Значение должно быть больше или ровно 2.")
-    @Max(value = 30, message = "Значение должно быть меньше или ровно 30.")
+    @Max(value = 30, message = "Ширина поля должна быть не менее 2 и не более 30.")
     private Integer width;
 
     @NotNull
     @Min(value = 2, message = "Значение должно быть больше или ровно 2.")
-    @Max(value = 30, message = "Значение должно быть меньше или ровно 30.")
+    @Max(value = 30, message = "Высота поля должна быть не менее 2 и не более 30.")
     private Integer height;
 
     @NotNull
     @Min(value = 1, message = "Значение должно быть больше или равно 1.")
+    @JsonProperty("mines_count")
     private Integer minesCount;
 }
